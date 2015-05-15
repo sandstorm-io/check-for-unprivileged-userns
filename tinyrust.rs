97db02aa251a11ff55ee63757af85ac65706a1c8
Asheesh Laroia <asheesh@asheesh.org>
2015-05-14 17:00:17 -0700

    Convert this into a user namespaces test program

--- tinyrust.rs
+++ tinyrust.rs
@@ -12,14 +12,51 @@ fn exit(n: usize) -> ! {
     }
 }
 
-fn write(fd: usize, buf: &[u8]) {
+fn write(fd: usize, buf: &[u8]) -> usize {
     unsafe {
-        syscall!(WRITE, fd, buf.as_ptr(), buf.len());
+        return syscall!(WRITE, fd, buf.as_ptr(), buf.len())
+    }
+}
+
+fn getuid() -> usize {
+    unsafe {
+        return syscall!(GETUID)
+    }
+}
+
+fn setuid(n: usize) -> usize {
+    unsafe {
+        return syscall!(SETUID, n)
+    }
+}
+
+fn unshare(n: usize) -> usize {
+    unsafe {
+        return syscall!(UNSHARE, n)
     }
 }
 
 #[no_mangle]
 pub fn main() {
+    // The purpose of main() is to verify that UID namespaces work
+    // when not root, so we make sure we're not root.
+
+    // We will:
+    //
+    // - exit(0) if we confirm user namespaces work great as non-root.
+    //
+    // - exit(1) if we confirm user namespaces do not work for us as
+    //   non-root.
+    //
+    // - exit(2) if we find ourselves in a situation where we have no
+    //   idea what is going on.
+
+    // Since I started this codebase by forking Keegan McAllister's
+    // tinyrust project, it comes bundled with a string constant
+    // ("Hello!\n"). We capture that message here, not because we use
+    // it in this program, but because while modifying this code, it
+    // can be helpful to be able to write() it for debugging's sake.
+
     // Make a Rust value representing the string constant we stashed
     // in the ELF file header.
     let message: &'static [u8] = unsafe {
@@ -29,6 +66,32 @@ pub fn main() {
         })
     };
 
-    write(1, message);
-    exit(0);
+    // First, check if we are root. If we are, we setuid to 65534
+    // (which is just a dummy non-root user ID, which also happens to
+    // usually be "nobody"), since we want to find out if
+    // unshare(CLONE_NEWUSER) works when we are not root.
+
+    let current_uid = getuid();
+    if (current_uid == 0) {
+        let setuid_result = setuid(65534);
+        if (setuid_result != 0) {
+            // Hmm, we failed to setuid properly. Abort now.
+            exit(2);
+        }
+    }
+
+    // Now that we're not root, we can try to unshare(CLONE_NEWUSER).
+    // CLONE_NEWUSER is 0x10000000.
+
+    let unshare_result = unshare(0x10000000);
+
+    if (unshare_result == 0) {
+        // Hooray! Success.
+        exit(0);
+    } else {
+        // Some kind of sadness resulted. This could be permission
+        // denied, or it could be a deeper failure of the syscall, for
+        // example, if the syscall is unavailable on this kernel.
+        exit(1);
+    }
 }
